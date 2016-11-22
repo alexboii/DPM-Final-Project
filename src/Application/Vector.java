50b6bef8f431c6868098247aaaaceccb363fe482
pepoandra <pepoandra@users.noreply.github.com>
2016-11-22 15:49:25 -0500

    Added origin of vector  + final destination

--- src/Application/Vector.java
+++ src/Application/Vector.java
@@ -4,10 +4,32 @@ public class Vector {
 	
 	private double distance;
 	private double angle;
+	private double initialX, initialY;
 	
-	public Vector(double distance, double angle){
+	public Vector(double distance, double angle, double initialX, double initialY){
 		this.distance = distance;
 		this.angle = angle;
+		this.initialX = initialX;
+		this.initialY = initialY;
+		
+	}
+	
+	
+	
+	public double getInitialY() {
+		return initialY;
+	}
+	
+	public void setInitialY(double y){
+		this.initialY = y;
+	}
+	
+	public double getInitialX() {
+		return initialX;
+	}
+	
+	public void setInitialX(double x){
+		this.initialX = x;
 	}
 	
 	public double getDistance() {
@@ -26,5 +48,15 @@ public class Vector {
 		this.angle = angle;
 	}
 
+	//return XY values of final position of vector
+	public double[] getFinalPosition(){
+		double[] position = new double[2];
+		
+		position[0]= this.initialX + Math.cos(Math.toRadians(this.angle));
+		position[0]= this.initialY + Math.sin(Math.toRadians(this.angle));
+
+		return position;
+	}
+	
 	
 }
